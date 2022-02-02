fd42b78b6687eaa9f986927f9baa26685232750b
Lee Siha <mlice1030@soongsil.ac.kr>
2022-02-02 22:09:36 +0900

    add weather API, icons(weather, loading)

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
 import * as Location from "expo-location";
 import React, { useEffect, useState } from "react";
-import { View, Text, Dimensions, StyleSheet, ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  Dimensions,
+  StyleSheet,
+  ScrollView,
+  ActivityIndicator,
+  Image,
+} from "react-native";
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
+const API_KEY = "e4336120ae95530ffae07f07132ff848"; //not supposed to do like this, but it's free API key so I just put it here. Again, this is not secure
 
 export default function App() {
   const [region, setRegion] = useState("Loading...");
+  const [weatherInfo, setWeatherInfo] = useState([]);
   const [districtNstreet, setDNS] = useState("");
-  const [location, setLocation] = useState();
   const [ok, setOk] = useState(true);
-  const ask = async () => {
+  const getWeather = async () => {
     const { granted } = await Location.requestForegroundPermissionsAsync();
     if (!granted) {
       setOk(false);
@@ -22,9 +31,14 @@ export default function App() {
     );
     setRegion(location[0].region);
     setDNS(location[0].district + ", " + location[0].street);
+    const weatherData = await fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely,hourly,alerts&appid=${API_KEY}&units=metric`
+    );
+    const json = await weatherData.json();
+    setWeatherInfo(json.daily);
   };
   useEffect(() => {
-    ask();
+    getWeather();
   }, []);
   return (
     <View style={styles.container}>
@@ -38,22 +52,39 @@ export default function App() {
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={styles.weather}
       >
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
+        {weatherInfo.length === 0 ? (
+          <View style={{ ...styles.day, alignItems: "center" }}>
+            {/* style={styles.day} 적용하고, 추가로 뒤에 효과도 넣음 */}
+            <ActivityIndicator size="large" color="black" />
+          </View>
+        ) : (
+          weatherInfo.map((day, index) => (
+            <View key={index} style={styles.day}>
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  width: "100%",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Text style={styles.temp}>
+                  {parseFloat(day.temp.day).toFixed(1)}
+                </Text>
+                <Image
+                  style={{ width: 100, height: 100 }}
+                  source={{
+                    uri: `http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`,
+                  }}
+                />
+              </View>
+              <Text style={styles.main}>{day.weather[0].main}</Text>
+              <Text style={styles.description}>
+                {day.weather[0].description}
+              </Text>
+            </View>
+          ))
+        )}
       </ScrollView>
     </View>
   );
@@ -76,15 +107,19 @@ const styles = StyleSheet.create({
   weather: {},
   day: {
     width: SCREEN_WIDTH,
-    alignItems: "center",
+    alignItems: "flex-start",
+    paddingHorizontal: 20,
   },
   temp: {
     marginTop: 50,
     fontWeight: "bold",
-    fontSize: 178,
+    fontSize: 125,
+  },
+  main: {
+    marginTop: -20,
+    fontSize: 50,
   },
   description: {
-    marginTop: -30,
-    fontSize: 60,
+    fontSize: 30,
   },
 });
--- Readme.md
+++ Readme.md
@@ -1,3 +1,4 @@
 # Nomad Weather
 
 Beautiful React Native Weather Application (using expo)
+I got the weather information from [here](https://openweathermap.org/api)
